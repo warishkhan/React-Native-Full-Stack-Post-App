319a3596d042edb7fd9d254e7de20fd05adb661d
warish khan <warishkhan790@gmail.com>
2023-10-03 22:52:20 +0530

    first commit

---
+++ client/App.js
@@ -0,0 +1,11 @@
+import RootNavigation from "./navigation";
+
+import { NavigationContainer } from "@react-navigation/native";
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <RootNavigation />
+    </NavigationContainer>
+  );
+}
